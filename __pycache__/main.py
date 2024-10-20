65777f1348adcfb2d99dc15b377138fa2a67b4e6
mouli181 <moulitharan2471@gmail.com>
2024-10-20 16:49:32 +0530

    added

---
+++ __pycache__/main.py
@@ -0,0 +1,5 @@
+# main.py
+import Python.CounterApp.my_module as my_module
+
+print(my_module.greet("Moulitharan"))
+print("Value of pi:", my_module.pi)
---
+++ __pycache__/my_module.py
@@ -0,0 +1,4 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+pi = 3.14159
